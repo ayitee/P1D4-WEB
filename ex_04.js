c8aba8b7bced1b1717d33607cf3c2536b338ec80
ayitee <matis.gaudin@epitech.digital>
2024-10-02 22:59:05 +0200

    Update ex_04.js

--- ex_04.js
+++ ex_04.js
@@ -1,1 +1,5 @@
-
+function capitalize(words) {
+  return words.map(word => word.toUpperCase());
+}
+// This function takes an array of words, converts each word to uppercase,
+// and returns a new array containing the capitalized words.
