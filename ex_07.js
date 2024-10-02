bea9c57af9d77ac22c01bb3e71c1ca2ce7d16eeb
ayitee <matis.gaudin@epitech.digital>
2024-10-02 23:11:21 +0200

    Update ex_07.js

--- ex_07.js
+++ ex_07.js
@@ -22,9 +22,5 @@ function addCharacteristics(country, cityName, housingName, ...characteristics) 
 // This code defines a 'country' object that acts as a container for cities.
 // The 'createCity' function adds a new city to the 'country' object, using the city name as a key and assigning it an empty object as its value.
 // The 'createHousing' function checks if the specified city exists in the 'country' object.
-// If the city exists, it adds a new property for the housing with the provided name, initializing it as an empty array to store characteristics.
-// The 'addCharacteristics' function takes multiple characteristics as arguments and adds them to the specified housing's array.
-// It checks if both the city and housing exist in the 'country' object before adding the characteristics.
-
 
 
