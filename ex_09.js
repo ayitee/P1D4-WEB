3f456b50219093dbd6c9676d4daed5ea20163049
ayitee <matis.gaudin@epitech.digital>
2024-10-02 22:17:38 +0200

    Update ex_09.js

--- ex_09.js
+++ ex_09.js
@@ -1,1 +1,10 @@
-
+function addHousing(housingList, newHouse, quantity) {
+  if (housingList.has(newHouse)) {
+    housingList.set(newHouse, housingList.get(newHouse) + quantity);
+  } else {
+    housingList.set(newHouse, quantity);
+  }
+}
+// This function checks if the housing map already contains the specified house type (newHouse). 
+// If it does, it increases the quantity by adding to the existing value.
+// If it doesn’t, it adds the new house type with the specified quantity.
