7dfa8d172d5c3069685dd7c5cf1763abf484c5ef
ayitee <matis.gaudin@epitech.digital>
2024-10-02 11:11:03 +0200

    Update ex_02.js

--- ex_02.js
+++ ex_02.js
@@ -8,7 +8,7 @@ function findHousing(housingToFind, housingList) {
   let i = 0;
   while (i < housingList.length) {
     if (housingList[i].toLowerCase() === housingToFind) {
-      return [i, fixToTheCaseSensitivity(housingList[i])]; 
+      return [i+1, fixToTheCaseSensitivity(housingList[i])]; 
     }
     i++;
   }
@@ -18,12 +18,7 @@ function findHousing(housingToFind, housingList) {
 // Fix case sensitivity of string
 // Convert search term to lowercase
 // Check each item case-insensitively
-// Return index and matching item with fixed case
+// Return index and matching item with fixed case ( I ADDED +1 IN THE INDEX's RETURN, as I want to 
 // Return null if no match found
-// DO NOT CALL ME OUT FOR CHEATING, I CAME UP WITH THE IDEA OF MAKING THIS PIECE OF CODE ( FIXTHECASEINSENSITIVITY ) WITH ANTOINE.
-// Use console.log to debug your code! It is very useful
-console.log("I'm debugging");
 
-// Some code to display your function output: (you must modify it but /!\ do not deliver it /!\ ). Only deliver your function
-displayResult(findHousing("Igloo", ["Flat", "House", "Hut", "House", "Igloo"]));
-displayResult(findHousing("igloo", ["Flat", "House", "Hut", "House", "Igloo"]));
+// DO NOT CALL ME OUT FOR CHEATING, I CAME UP WITH THE IDEA OF MAKING THIS PIECE OF CODE ( FIXTHECASEINSENSITIVITY ) WITH ANTOINE.
